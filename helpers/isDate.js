db2cd007da6bdc5eeb4922b3adf0516fc87e8426
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2023-11-12 09:45:02 -0300

    CRUD completo de los eventos

---
+++ helpers/isDate.js
@@ -0,0 +1,19 @@
+//Custom Validator
+const moment = require('moment')
+const isDate = ( value) => {
+
+    if(!value){
+        return false
+    }
+    const fecha = moment(value);
+    if(fecha.isValid()){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+module.exports = {
+    isDate
+}
--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,10 @@
 const {Router} = require('express');
+const { check} = require('express-validator')
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
+
 const router = Router();
 
 router.use(validarJWT); //Realiza la validacion de token en TODAS LAS RUTAS - 
@@ -8,7 +12,14 @@ router.use(validarJWT); //Realiza la validacion de token en TODAS LAS RUTAS -
 router.get('/', getEventos)
 
 
-router.post('/', crearEvento)
+router.post('/',
+[
+    check('title', 'El titulo es oblogatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalizacion es obligatoria').custom(isDate),
+    validarCampos
+],
+ crearEvento)
 
 
 router.put('/:id', actualizarEvento)
